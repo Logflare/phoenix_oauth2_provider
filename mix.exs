d94646bae043694e3c598564151fa943372aaef7
TzeYiing <ty@tzeyiing.com>
2023-05-12 01:26:41 +0800

    chore: use < 1.8 phoenix

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule PhoenixOauth2Provider.Mixfile do
   defp deps do
     [
       {:ex_oauth2_provider, "~> 0.5.1"},
-      {:phoenix, ">= 1.5.0 and <= 1.7.2"},
+      {:phoenix, ">= 1.5.0 and < 1.8.0"},
       {:phoenix_view, "~> 2.0.0"},
       {:phoenix_html, "~> 3.0.0"},
       {:phoenix_ecto, "~> 4.3.0", only: [:test, :dev]},
